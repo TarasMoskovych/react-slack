48269a4d05ea625e8ba3bfa5030ec6151598fdc2
Taras Moskovych <moskovych.taras@gmail.com>
2019-12-29 19:43:35 +0200

    prepare statefull components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Grid } from 'semantic-ui-react';
 
-function App() {
+import ColorPanel from './ColorPanel/ColorPanel';
+import SidePanel from './SidePanel/SidePanel';
+import Messages from './Messages/Messages';
+import MetaPanel from './MetaPanel/MetaPanel';
+
+const App = () => {
   return (
-    <div>
-     Root
-    </div>
+    <Grid columns="equal" className="app" style={{ background: '#eee' }}>
+      <ColorPanel/>
+      <SidePanel/>
+      <Grid.Column style={{ marginLeft: 320 }}>
+        <Messages/>
+      </Grid.Column>
+      <Grid.Column width={4}>
+        <MetaPanel/>
+      </Grid.Column>
+    </Grid>
   );
-}
+};
 
 export default App;
